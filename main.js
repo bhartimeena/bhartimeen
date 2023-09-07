a5b511c74c00bfe923b1665db74066ce9bcc5c06
Bharti <bharti@beanbyte.in>
2023-09-07 13:48:54 +0530

    Header and home section created.

---
+++ main.js
@@ -0,0 +1,16 @@
+var typed = new Typed(".text", {
+    strings:['Frontend Developer.', 'Web Developer.', "Software Developer."],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+});
+
+
+var typed = new Typed(".text1", {
+    strings:['Hello', 'Welcome', "Here."],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+});
